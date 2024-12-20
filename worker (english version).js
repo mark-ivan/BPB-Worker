8aa80538c2779df3b04a33cfdca87e64c060f9f7
mark-ivan <71008470+mark-ivan@users.noreply.github.com>
2024-12-20 22:22:41 +0800

    Update worker (english version).js

--- worker (english version).js
+++ worker (english version).js
@@ -9448,10 +9448,10 @@ js-sha256/src/sha256.js:
   (**
    * [js-sha256]{@link https://github.com/emn178/js-sha256}
    *
-   * @version 0.11.0
-   * @author Chen, Yi-Cyuan [emn178@gmail.com]
-   * @copyright Chen, Yi-Cyuan 2014-2024
-   * @license MIT
+   * @version 2.7.4
+   * @author 
+   * @copyright 
+   * @license 
    *)
 */
 //# sourceMappingURL=worker.js.map
